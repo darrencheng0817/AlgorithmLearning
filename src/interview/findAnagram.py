84fdc3d2d07d36fbf3db8ee02c2cc14b239eaa74
Darren Cheng <darrencheng0817@gmail.com>
2015-12-02 11:46:58 -0800

    findAnagram

--- src/interview/findAnagram.py
+++ src/interview/findAnagram.py
@@ -3,3 +3,36 @@ Created on 2015年12月1日
 给定字符串A，B， 判断A中是否存在子串是B的anagram
 @author: Darren
 '''
+def findAnagram(a,b):
+    db={}
+    for char in b:
+        if char in db:
+            db[char]+=1
+        else:
+            db[char]=1
+    index=0
+    da={}
+    while index<len(b):
+        char=a[index]
+        if char in da:
+            da[char]+=1
+        else:
+            da[char]=1
+        index+=1
+    while index<len(a):
+        if da==db:
+            return index-len(b)
+        if a[index]!=a[index-len(b)]:
+            if da[a[index-len(b)]]==1:
+                da.pop(a[index-len(b)])
+            else:
+                da[a[index-len(b)]]-=1
+            if a[index] in da:
+                da[a[index]]+=1
+            else:
+                da[a[index]]=1
+        index+=1
+        
+a="ajdhgkajhg"
+b="hdkg"
+print(findAnagram(a, b))
