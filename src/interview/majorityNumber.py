743554b36def56d2158edd18a63a04f4aa013489
darrencheng0817 <darrencheng0817@gmail.com>
2015-12-04 21:37:17 -0800

    another practice 12/04/2015

--- src/interview/majorityNumber.py
+++ src/interview/majorityNumber.py
@@ -3,3 +3,19 @@ Created on 2015年12月1日
 数组里一个数的出现次数超过数组长度的50%， 找到这个数
 @author: Darren
 '''
+
+def findMajorityNum(nums):
+    if not nums:
+        raise Exception("Empty Input!")
+    candidate=0
+    count=0
+    for index in range(len(nums)):
+        if count==0:
+            candidate=nums[index]
+            count+=1
+        else:
+            if candidate==nums[index]:
+                count+=1
+            else:
+                count-=1
+    return candidate
