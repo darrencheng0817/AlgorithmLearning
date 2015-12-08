04a17fd8d5ba90f8869e08f8f993375b8c148310
darrencheng0817 <darrencheng0817@gmail.com>
2015-12-08 00:22:58 -0800

    12/08 night

--- src/interview/minDistancePair.py
+++ src/interview/minDistancePair.py
@@ -1,6 +1,6 @@
 '''
 Created on 2015年12月1日
-给一个整数n，输出俩数x和y，使得x*y的值在 [n, n+2] 的范围内，同时保证 |x - y|  
+给一个整数n，输出俩数x和y，使得x*y的值在 [n, n+2] 的范围内，同时保证 |x - y|  最小
 从x=y=sqrt(n+2) 开始搜索，第一次碰到一对x和y，能满足[n,  n+2], 那这对x y就是要返回的结果
 int[] searchXandY(int n) {
                 int[] result = new int[2];
@@ -27,3 +27,4 @@ int[] searchXandY(int n) {
         }
 @author: Darren
 '''
+
