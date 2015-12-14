479b61c4fa2a1a8ba2518a7c6ab9190dff86fafa
darrencheng0817 <darrencheng0817@gmail.com>
2015-12-13 23:27:19 -0800

    updated lowestCommonAncestor

--- Python/interview/lowestCommonAncestor.py
+++ Python/interview/lowestCommonAncestor.py
@@ -3,8 +3,8 @@ Created on 2015年12月1日
 https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
 @author: Darren
 '''
-from Pyhton.models.BTNode import *
-
+import models
+from models.BTNode import *
 
 def findLCA(root,node1,node2):
     if not root:
@@ -20,6 +20,33 @@ def findLCA(root,node1,node2):
     else: 
         return right
 
+def findLCA2(root,value1,value2):
+    '''
+    if node1 is the ancestor of node 2, return node1's lowest ancestor
+    '''
+    res=[]
+    targets=set([value1,value2])
+    findLCA2Util(root, targets, res, [])
+    index=0
+    while index<len(res[0]) and index<len(res[1]):
+        if res[0][index]!=res[1][index]:
+            return res[0][index-1]
+        index+=1
+    if index==len(res[0]):
+        return res[0][-1]
+    if index==len(res[1]):
+        return res[1][-1]    
+
+def findLCA2Util(root,targets,res,path):
+    if not root:
+        return
+    if root.value in targets:
+        res.append(path)
+        targets.remove(root.value)
+        if not targets:
+            return
+    findLCA2Util(root.left, targets, res, path+[root.value])
+    findLCA2Util(root.right, targets, res, path+[root.value])
 root=BTNode(4)
 root.left=BTNode(2)
 root.right=BTNode(6)
@@ -30,3 +57,4 @@ root.right.right=BTNode(7)
 
 printTreeLevelOrder(root)
 print(findLCA(root, root.left.left, root.left.right))
+print(findLCA2(root, 1, 2))
