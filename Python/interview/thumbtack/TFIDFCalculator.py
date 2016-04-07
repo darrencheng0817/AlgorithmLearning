c11254668e67c35cf49cf89559f1f88b126bee74
Darren Cheng <darrencheng0817@gmail.com>
2016-04-06 23:51:22 -0700

    thumbtack

---
+++ Python/interview/thumbtack/TFIDFCalculator.py
@@ -0,0 +1,62 @@
+'''
+Created on 2016年3月1日
+
+@author: Darren
+'''
+from math import log
+import unittest
+
+class TFIDFCalculator(object):
+    
+    def search(self,query,cropus):
+        if not cropus:
+            raise Exception("Empty cropus")
+        totalDoc=len(cropus)
+        wordsStatistic={}
+        docStatistic={}
+        for docName in cropus.keys():
+            wordsList=cropus[docName]
+            wordCount=0
+            for word in wordsList:
+                wordCount+=1
+                if word not in wordsStatistic:
+                    wordsStatistic[word]={}
+                if docName not in wordsStatistic[word]:
+                    wordsStatistic[word][docName]=0
+                wordsStatistic[word][docName]+=1
+            docStatistic[docName]=wordCount    
+        maxTFIDF=0
+        res=""
+        print(docStatistic)
+        print(wordsStatistic)
+        for docName in cropus:
+            TFIDFValue=0
+            for queryWord in query:
+                if docName not in wordsStatistic[queryWord]:
+                    continue
+                TF=wordsStatistic[queryWord][docName]/docStatistic[docName]
+                IDF=log(totalDoc/len(wordsStatistic[queryWord]))
+                TFIDFValue+=TF*IDF
+            if TFIDFValue>maxTFIDF:
+                maxTFIDF=TFIDFValue
+                res=docName
+            print(docName,TFIDFValue)
+        return res 
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.testClass=TFIDFCalculator()
+        
+    def test_search(self):
+        query=["interview","with", "thumbtack"]
+        cropus={"doc1":["interview", "with", "thumbtack"],
+                "doc2":["happy"],
+                "doc3":["interview"]
+                }
+        expectedResult="doc1"
+        actualReslut=self.testClass.search(query, cropus)
+        self.assertEqual(expectedResult, actualReslut)
+        
+unittest.main()
+        
--- Python/leetcode/AddBinary.py
+++ Python/leetcode/AddBinary.py
@@ -2,15 +2,33 @@
 Created on 1.12.2016
 
 @author: Darren
-''''''
-
+'''
+'''
 Given two binary strings, return their sum (also a binary string).
-
-
-
 For example,
 a = "11"
 b = "1"
 Return "100".
 " 
 '''
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        carry=0
+        res=""
+        for index in range(max(len(a),len(b))):
+            da,db=0,0
+            if len(a)-index-1>=0:
+                da=int(a[len(a)-index-1])
+            if len(b)-index-1>=0:
+                db=int(b[len(b)-index-1])
+            sum_d=da+db+carry
+            carry=sum_d//2
+            res=str(sum_d%2)+res
+        if carry==1:
+            res="1"+res
+        return res
